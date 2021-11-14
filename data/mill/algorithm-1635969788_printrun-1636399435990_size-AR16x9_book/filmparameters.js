71a536df85a3bd3c69a4fe394d3780065d335982
mctavish <kathy@mctavish.io>
2021-11-13 20:06:13 -0600

    added demo files

---
+++ data/mill/algorithm-1635969788_printrun-1636399435990_size-AR16x9_book/filmparameters.js
@@ -0,0 +1,1154 @@
+[
+	{
+		"file": "coreimage-000001.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000002.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000003.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000004.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000005.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000006.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000007.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000008.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000009.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000010.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000011.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000012.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000013.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000014.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000015.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000016.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000017.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000018.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000019.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000020.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000021.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000022.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000023.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000024.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000025.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000026.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000027.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000028.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000029.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000030.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000031.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000032.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000033.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000034.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000035.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000036.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000037.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000038.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000039.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000040.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000041.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000042.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000043.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000044.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000045.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000046.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000047.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000048.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000001.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000002.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000003.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000004.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000005.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000006.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000007.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000008.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000009.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000010.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000011.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000012.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000013.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000014.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000015.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000016.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000017.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000018.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000019.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000020.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000021.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000022.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000023.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000024.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000025.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000026.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000027.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000028.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000029.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000030.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000031.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000032.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000033.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000034.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000035.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000036.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000037.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000038.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000039.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000040.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000041.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000042.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000043.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000044.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000045.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000046.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000047.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000048.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000001.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000002.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000003.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000004.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000005.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000006.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000007.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000008.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000009.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000010.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000011.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000012.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000013.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000014.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000015.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000016.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000017.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000018.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000019.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000020.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000021.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000022.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000023.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000024.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000025.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000026.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000027.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000028.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000029.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000030.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000031.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000032.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000033.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000034.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000035.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000036.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000037.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000038.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000039.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000040.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000041.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000042.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000043.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000044.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000045.png",
+		"n": 5
+	},
+	{
+		"file": "coreimage-000046.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000047.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000048.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000001.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000002.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000003.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000004.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000005.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000006.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000007.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000008.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000009.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000010.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000011.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000012.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000013.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000014.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000015.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000016.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000017.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000018.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000019.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000020.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000021.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000022.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000023.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000024.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000025.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000026.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000027.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000028.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000029.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000030.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000031.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000032.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000033.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000034.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000035.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000036.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000037.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000038.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000039.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000040.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000041.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000042.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000043.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000044.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000045.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000046.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000047.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000048.png",
+		"n": 4
+	},
+	{
+		"file": "coreimage-000001.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000002.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000003.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000004.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000005.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000006.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000007.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000008.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000009.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000010.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000011.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000012.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000013.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000014.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000015.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000016.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000017.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000018.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000019.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000020.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000021.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000022.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000023.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000024.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000025.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000026.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000027.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000028.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000029.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000030.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000031.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000032.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000033.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000034.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000035.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000036.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000037.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000038.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000039.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000040.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000041.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000042.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000043.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000044.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000045.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000046.png",
+		"n": 3
+	},
+	{
+		"file": "coreimage-000047.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000048.png",
+		"n": 2
+	},
+	{
+		"file": "coreimage-000001.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000002.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000003.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000004.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000005.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000006.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000007.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000008.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000009.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000010.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000011.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000012.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000013.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000014.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000015.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000016.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000017.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000018.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000019.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000020.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000021.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000022.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000023.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000024.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000025.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000026.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000027.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000028.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000029.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000030.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000031.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000032.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000033.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000034.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000035.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000036.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000037.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000038.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000039.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000040.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000041.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000042.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000043.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000044.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000045.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000046.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000047.png",
+		"n": 1
+	},
+	{
+		"file": "coreimage-000048.png",
+		"n": 1
+	}
+]
---
+++ data/mill/algorithm-1635998398_printrun-1636422397165_size-AR16x9_book/algorithm-1635998398_printrun-1636422397165_size-AR16x9_processCommands.sh
@@ -0,0 +1,18 @@
+for i in *.pdf; do magick convert $i -resize 1920 $i.png; done;
+for file in *pdf.png; do mv "$file" "${file/.pdf.png/.png}"; done;
+for i in *.pdf; do magick convert $i $i.tif; done
+for file in *pdf.tif; do mv "$file" "${file/.pdf.tif/.tif}"; done;
+cd ..
+node createCoreImages ./algorithm-1635998398_printrun-1636422397165_size-AR16x9_book
+node createFilm ./algorithm-1635998398_printrun-1636422397165_size-AR16x9_book
+cd ./algorithm-1635998398_printrun-1636422397165_size-AR16x9_book
+ffmpeg -r 24 -f image2  -s 1920x1080 -start_number 01 -i algorithm-1635998398_printrun-1636422397165_size-AR16x9%02d.png algorithm-1635998398_printrun-1636422397165_size-AR16x9.webm
+ffmpeg -r 24 -f image2  -s 1920x1080 -start_number 000001 -i image-%06d.png algorithm-1635998398_printrun-1636422397165_size-AR16x9_pulse.webm
+rm coreimage*.png
+for i in algorithm*.png; do magick convert $i -resize 640 $i.small.png; done;
+for file in *png.small.png; do mv "$file" "${file/.png.small.png/-small.png}"; done;
+pdfunite *.pdf algorithm-1635998398_printrun-1636422397165_size-AR16x9.pdf
+# quicktime: open an image sequence with image-* files and save as algorithm-1635998398_printrun-1636422397165_size-AR16x9_pulse.mov
+# rm image-*.png
+# quicktime: open an image sequence with algorithm-1635998398_printrun-1636422397165_size-AR16x9 files order by date created and save as algorithm-1635998398_printrun-1636422397165_size-AR16x9.mov
+	
